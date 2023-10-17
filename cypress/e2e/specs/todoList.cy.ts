7e0786b861df9185615859446f689af18bf83fae
Grzegorz Cagara <grzegorzcagara@gmail.com>
2023-10-17 22:24:40 +0200

    *adding proper descriptions

--- cypress/e2e/specs/todoList.cy.ts
+++ cypress/e2e/specs/todoList.cy.ts
@@ -1,8 +1,8 @@
 import dsl from '../../dsl/dsl'
 import { checkIfTitleIsDisplayed, checkIfUserLandsOnProperUrl } from '../checks/common'
 
-describe('template spec', () => {
-  it('passes', () => {
+describe('app has basic functionality', () => {
+  it('should load', () => {
     dsl
       .openToDoApp()
       .verify(checkIfUserLandsOnProperUrl(Cypress.config('baseUrl')))
